da622915fcace20ddc7bb5edf20a1df57c71d4dd
Marcello De Giorgi <marcello.degiorgi@kantar.com>
2026-03-01 18:12:18 +0100

    chore: rethink db data model

--- db.py
+++ db.py
@@ -1,21 +1,88 @@
-from sqlalchemy import create_engine, Column, Integer, String, Date, ForeignKey
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import (
+    create_engine,
+    Column,
+    Integer,
+    String,
+    Date,
+    ForeignKey,
+    Boolean,
+    insert,
+    select
+)
+from sqlalchemy.orm import declarative_base, Session
+from datetime import datetime
 
-engine = create_engine('sqlite:///biliardino.db')
+engine = create_engine("sqlite:///biliardino.db")
 Base = declarative_base()
 
-class Player(Base):
-    __tablename__ = 'player'
-    
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    elo = Column(Integer)
 
+class Player(Base):
+    __tablename__ = "player"
+
+    name = Column(String, primary_key=True)
+    create_at = Column(Date, default=datetime.today())
+    elo = Column(Integer, default=2000)
+
+
+class Match(Base):
+    __tablename__ = "match"
+
+    id = Column(Integer, primary_key=True)
+    date = Column(Date, default=datetime.today())
+    score_team_A = Column(Integer)
+    score_team_B = Column(Integer)
+
+
+class MatchPartecipant(Base):
+    __tablename__ = "match_partecipant"
+
+    id = Column(Integer, primary_key=True)
+    match_id = Column(ForeignKey(Match.id))
+    team_id = Column(String)
+    player_id = Column(ForeignKey(Player.name))
+    score_in_match = Column(Integer)
+    is_winner = Column(Boolean)
 
 
 Base.metadata.create_all(engine)
 
+def get_current_player_elo(player_id: str):
+    with Session(engine) as session:
+        with session.begin():
+            qry = select(Player.elo).where(Player.name == player_id)
+            return session.execute(qry)[0]
+
+def get_players():
+    with Session(engine) as session:
+        with session.begin():
+            players = session.query(Player.name).all()
+            return [p[0] for p in players]
 
 
+def insert_player(player_name: str):
+    with Session(engine) as session:
+        p =Player(name=player_name)
+        session.add(p)
+        session.commit()
 
 
+def insert_match(score_A: int, score_B: int) -> int:
+    with Session(engine) as session:
+        match = Match(score_team_A=score_A, score_team_B=score_B)
+        session.add(match)
+        session.commit()
+        return match.id
+
+def insert_match_partecipant(
+    team_id: str, match_id: str, player_id, score_in_match: int, is_winner: bool
+):
+    with Session(engine) as session:
+        mp = MatchPartecipant(
+                team_id=team_id,
+                match_id=match_id,
+                player_id=player_id,
+                is_winner=is_winner,
+                score_in_match=score_in_match,
+            )
+        session.add(mp)
+        session.commit()
--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import asyncio
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters, ConversationHandler, CallbackQueryHandler
+from telegram.ext import (
+    ApplicationBuilder,
+    ContextTypes,
+    CommandHandler,
+    MessageHandler,
+    filters,
+    ConversationHandler,
+    CallbackQueryHandler,
+)
 import dotenv
 import os
-from sqlalchemy.orm import Session
-from sqlalchemy import insert
-from db import engine, Player, Match
+from db import get_players, insert_player, insert_match, insert_match_partecipant
 from functools import partial
-from enum import Enum
+from datetime import datetime
 
 A1 = 0
 A2 = 1
-B1 = 2 
+B1 = 2
 B2 = 3
 SCORE = 4
 END = 5
@@ -19,35 +25,54 @@ END = 5
 dotenv.load_dotenv()
 TOKEN = os.environ.get("BOT_TOKEN")
 
-REGISTERED_PLAYERS = ["Alice", "Bob", "Charlie", "Derek", "Pino", "Gino", "Richard", "Legolas"]
 
-def get_player_keyboard(players:list[str]):
-    keyboard = []    
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!"
+    )
+
+def get_score_keyboard():
+    buttons = [InlineKeyboardButton(str(i), callback_data=str(i)) for i in range(10)]
+    
+    grid = [buttons[i:i + 3] for i in range(0, len(buttons), 3)]
+    return InlineKeyboardMarkup(grid)
+
+def get_player_keyboard(players: list[str]):
+    keyboard = []
     for i in range(0, len(players), 2):
-        row = [InlineKeyboardButton(p, callback_data=p) for p in players[i:i+2]]
+        row = [InlineKeyboardButton(p, callback_data=p) for p in players[i : i + 2]]
         keyboard.append(row)
     return InlineKeyboardMarkup(keyboard)
 
+
 def get_teams_keyboard(data: dict):
-    keyboard = []    
+    keyboard = []
     teams = {"A": f"Team A\n {data['A']}", "B": f"Team B\n {data['B']}"}
-     
-    row = [InlineKeyboardButton(v, callback_data=k) for k,v in teams.items()]
+
+    row = [InlineKeyboardButton(v, callback_data=k) for k, v in teams.items()]
     keyboard.append(row)
     return InlineKeyboardMarkup(keyboard)
 
 
 async def start_match(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.user_data["players"] = get_players()
     await update.message.reply_text(
-        "Who is Player 1 for Team A?", 
-        reply_markup=get_player_keyboard(REGISTERED_PLAYERS)
+        "Who is Player 1 for Team A?",
+        reply_markup=get_player_keyboard(context.user_data["players"]),
     )
     return 0
 
 
-async def get_player(update:Update, context:ContextTypes.DEFAULT_TYPE, team: str, state_id: int, text: str = "", is_last_player: bool = False):
+async def get_player(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE,
+    team: str,
+    state_id: int,
+    text: str = "",
+    is_last_player: bool = False,
+):
     query = update.callback_query
-    await query.answer() 
+    await query.answer()
     data = context.user_data
     player_name = query.data
     data[state_id] = player_name
@@ -58,19 +83,21 @@ async def get_player(update:Update, context:ContextTypes.DEFAULT_TYPE, team: str
 
     if not is_last_player:
         selected_players = data.get("A", []) + data.get("B", [])
-        available_players = list(set(REGISTERED_PLAYERS) - set(selected_players))
+        available_players = list(set(data["players"]) - set(selected_players))
         msg = f"Selected players: {', '.join(selected_players)}.\n\n" + text
         await query.edit_message_text(
-            msg,
-            reply_markup=get_player_keyboard(available_players)
+            msg, reply_markup=get_player_keyboard(available_players)
         )
     else:
-        await query.edit_message_text(f"Who won?", reply_markup=get_teams_keyboard(data))
+        await query.edit_message_text(
+            "Who won?", reply_markup=get_teams_keyboard(data)
+        )
     return state_id + 1
 
-async def get_score(update:Update, context:ContextTypes.DEFAULT_TYPE):
+
+async def get_score(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
-    await query.answer() 
+    await query.answer()
     data = context.user_data
     team = query.data
 
@@ -78,53 +105,67 @@ async def get_score(update:Update, context:ContextTypes.DEFAULT_TYPE):
         data["score_A"] = 10
     else:
         data["score_B"] = 10
-    await query.edit_message_text(f"Winning team: {team}🏆\n\nWhat was the score of the losing team?")
-    return END
-
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="I'm a bot, please talk to me!"
+    await query.edit_message_text(
+        f"Winning team: {team}🏆\n\nWhat was the score of the losing team?",
+        reply_markup=get_score_keyboard()
     )
-
+    return END
 
 
 async def add_player(update: Update, context: ContextTypes.DEFAULT_TYPE):
     player = context.args[0]
+    players = get_players()
 
-    with Session(engine) as session:
-        with session.begin():
-            players = session.query(Player.name).all()
-            players = [p[0] for p in players]
-            if player in players:
-                await context.bot.send_message(chat_id=update.effective_chat.id, text="Player {} already exists!".format(player))
-            else:
-                stmt = insert(Player).values(name=player, elo=2000)
-                session.execute(stmt)
-                await context.bot.send_message(chat_id=update.effective_chat.id, text="Player {} added".format(player))
+    if player in players:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="Player {} already exists!".format(player),
+        )
+    else:
+        insert_player(player)
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id, text="Player {} added".format(player)
+        )
 
 
 async def finish(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+    fail_team_score = int(query.data)
     data = context.user_data
-    fail_team_score = update.message.text
-    data = context.user_data
+
+    elo_prize = 42 # change
 
     if "score_A" in data:
         data["score_B"] = fail_team_score
+        winner = "A"
+        elo_prize_A = elo_prize
+        elo_prize_B = -elo_prize
+
     else:
         data["score_A"] = fail_team_score
+        winner = "B"
+        elo_prize_A = -elo_prize
+        elo_prize_B = elo_prize
+
+    match_id = insert_match(score_A = data["score_A"], score_B=data["score_B"])
+    A_list = [("A", match_id, p, elo_prize_A, True if winner == "A" else False) for p in data["A"]]
+    B_list = [("B", match_id, p, elo_prize_B, True if winner == "B" else False) for p in data["B"]]
     
+    for l in A_list + B_list:
+        insert_match_partecipant(l[0], l[1], l[2], l[3], l[4])
+
     summary = (
         f"Match Recorded! 🏆\n"
         f"Team A: {', '.join(data['A'])}\n"
         f"Team B: {', '.join(data['B'])}\n"
         f"Score: {data['score_A']}-{data['score_B']}"
     )
-    
-    await update.message.reply_text(summary)
-    context.user_data.clear() 
+
+    await query.edit_message_text(summary)
+    context.user_data.clear()
     return ConversationHandler.END
+
 
 async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
     context.user_data.clear()
@@ -132,16 +173,22 @@ async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
     return ConversationHandler.END
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     application = ApplicationBuilder().token(TOKEN).build()
-    
-    start_handler = CommandHandler('start', start)
-    add_handler = CommandHandler('add_player', add_player)
 
-    get_p1a = partial(get_player, state_id = A1, team="A", text = "Who is the player 2 of team A?")
-    get_p2a = partial(get_player, state_id = A2, team="A", text = "Who is the player 1 of team B?")
-    get_p1b = partial(get_player, state_id = B1, team="B", text = "Who is the player 2 of team B?")
-    get_p2b = partial(get_player, state_id = B2, team="B", is_last_player=True)
+    start_handler = CommandHandler("start", start)
+    add_handler = CommandHandler("add_player", add_player)
+
+    get_p1a = partial(
+        get_player, state_id=A1, team="A", text="Who is the player 2 of team A?"
+    )
+    get_p2a = partial(
+        get_player, state_id=A2, team="A", text="Who is the player 1 of team B?"
+    )
+    get_p1b = partial(
+        get_player, state_id=B1, team="B", text="Who is the player 2 of team B?"
+    )
+    get_p2b = partial(get_player, state_id=B2, team="B", is_last_player=True)
 
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("addmatch", start_match)],
@@ -151,14 +198,13 @@ if __name__ == '__main__':
             B1: [CallbackQueryHandler(get_p1b)],
             B2: [CallbackQueryHandler(get_p2b)],
             SCORE: [CallbackQueryHandler(get_score)],
-            END: [MessageHandler(filters.TEXT & ~filters.COMMAND, finish)],
+            END: [CallbackQueryHandler(finish)],
         },
         fallbacks=[CommandHandler("cancel", cancel)],
-)
+    )
 
     application.add_handler(start_handler)
     application.add_handler(add_handler)
     application.add_handler(conv_handler)
 
-    
     application.run_polling()
